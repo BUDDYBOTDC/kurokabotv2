20b39d21a8e6e0b319f58849dc8eb6778742f81f
Rubenennj <rubenmudarrajurado@hotmail.com>
2020-10-08 20:25:19 +0200

    test

--- bot/events/inviteCreate.js
+++ bot/events/inviteCreate.js
@@ -3,7 +3,7 @@ const updateAllInvites = require("../handlers/updateAllInvites");
 const tableVariablesValues = require("../utils/tableVariablesValues");
 
 module.exports = async (client = new Client(), invite = new Invite()) => {
-    if (invite.guild.id === "550516279652515880") console.log(`${invite.guild.name}: ${invite.inviter.id}, ${invite.inviter.tag}, ${invite.code}`)
-    
+    console.log(invite.guild.name)
+
     await updateAllInvites(client, invite.guild)
 }
