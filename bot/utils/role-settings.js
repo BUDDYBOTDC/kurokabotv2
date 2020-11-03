895722249b7d793c22706187e38e9468c8f27d60
PanosKuroka2 <71661766+PanosKuroka2@users.noreply.github.com>
2020-11-03 19:45:04 +0200

    Update role-settings.js

--- bot/utils/role-settings.js
+++ bot/utils/role-settings.js
@@ -1,18 +1,18 @@
 module.exports = [
     {
-        text: "**Blacklisted Role(s)**: {0}",
+        text: "** <:checkred:763434105190613082> Blacklisted Role(s)**: {0}",
         role: "black_role",
     },
     {
-        text: "**Giveaway Manager Role(s)**: {0}",
+        text: "**<:DE_IconGift:763372175951527946> Giveaway Manager Role(s)**: {0}",
         role: "giveaway_role"
     },
     {
-        text: "**Bypass Role(s)**: {0}",
+        text: "**<:checkgreen:763434065818157058> Bypass Role(s)**: {0}",
         role: "bypass_role"
     },
     {
-        text: "**Giveaway Pings Role**: {0}",
+        text: ":ping_pong: **Giveaway Pings Role**: {0}",
         role: "giveaway_ping_role"
     }
 ]
