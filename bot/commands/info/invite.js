62111754a2311d1a0002033d3e343d2c70036ca9
PanosKuroka2 <71661766+PanosKuroka2@users.noreply.github.com>
2020-11-05 17:34:32 +0200

    Update invite.js

--- bot/commands/info/invite.js
+++ bot/commands/info/invite.js
@@ -15,7 +15,7 @@ module.exports = {
         .setAuthor(`Invite ${client.user.username}`, message.author.displayAvatarURL({dynamic:true}), "https://discord.gg/f7MCvQJ")
         .setThumbnail(client.owner.displayAvatarURL({dynamic:true}))
         .setDescription( 
-            `:link: | Administrator Invite ( recommended ) \n <https://discord.com/oauth2/authorize?client_id=754024463137243206&scope=bot&permissions=8> \n :link: | Basic Permissions Invite \n <https://discord.com/oauth2/authorize?client_id=754024463137243206&scope=bot&permissions=388192> \n  :link: | Website: <https://kurokabots.com> \n :link: | Twitter: <https://twitter.com/KurokaBot>`
+            `:link: | Administrator Invite ( recommended ) \n https://discord.com/oauth2/authorize?client_id=754024463137243206&scope=bot&permissions=8 \n :link: | Basic Permissions Invite \n https://discord.com/oauth2/authorize?client_id=754024463137243206&scope=bot&permissions=388192`
         )
         .setFooter(`Made by Droplet Development - https://droplet.gg/discord`)
 
