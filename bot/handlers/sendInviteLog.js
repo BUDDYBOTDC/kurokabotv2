06b4844524f1c6701340344bb57fde880187fb6a
PanosKuroka2 <71661766+PanosKuroka2@users.noreply.github.com>
2020-11-06 08:54:13 +0200

    Update sendInviteLog.js

--- bot/handlers/sendInviteLog.js
+++ bot/handlers/sendInviteLog.js
@@ -34,7 +34,7 @@ module.exports = async (client = new Client(), invite, inviter, target) => {
             embed.setColor("GREEN")
             embed.setAuthor(`${target.user.tag} joined the guild:`, target.user.displayAvatarURL({dynamic:true}))
             embed.setThumbnail(client.user.displayAvatarURL())
-            embed.setDescription(`They were invited by ${inviter.tag}, by using the code \`${invite.code}\` and they now have ${data.invites_real || 0} invites!`)
+            embed.setDescription(`They were invited by ${inviter.tag}, by using the code \`${invite.code}\`!`)
 
         }
 
