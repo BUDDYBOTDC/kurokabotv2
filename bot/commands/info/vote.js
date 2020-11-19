c081b8de71403aa075759f912f209e1e6cd90758
PanosKuroka2 <71661766+PanosKuroka2@users.noreply.github.com>
2020-11-19 11:39:31 +0200

    Update vote.js

--- bot/commands/info/vote.js
+++ bot/commands/info/vote.js
@@ -13,8 +13,8 @@ module.exports = {
         const embed = new MessageEmbed()
         .setColor(customEmbed)
         .setTitle(`Vote ${client.user.username} here`)
-        .setURL("https://discord.boats/bot/754024463137243206/vote")
-        .setDescription(`By voting [here](https://discord.boats/bot/754024463137243206/vote) you won't get anything at all, but remember the bot's completely free and we'd like our users to value our effort put into this bot by making it full free!`)
+        .setURL("https://discord.boats/bot/kuroka")
+        .setDescription(`By voting [here](https://discord.boats/bot/kuroka) you won't get anything at all, but remember the bot's completely free and we'd like our users to value our effort put into this bot by making it full free!`)
         .setThumbnail(client.user.displayAvatarURL())
 
         message.channel.send(embed)
